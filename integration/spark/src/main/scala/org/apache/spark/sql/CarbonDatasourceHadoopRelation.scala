a40ccc8968221f2cfccf7c79b669cef4563f8bd5
kunal642 <kunalkapoor642@gmail.com>
2020-06-08 18:06:31 +0530

    filter_seek

--- integration/spark/src/main/scala/org/apache/spark/sql/CarbonDatasourceHadoopRelation.scala
+++ integration/spark/src/main/scala/org/apache/spark/sql/CarbonDatasourceHadoopRelation.scala
@@ -27,7 +27,7 @@ import org.apache.spark.sql.execution.command.management.CarbonInsertIntoCommand
 import org.apache.spark.sql.execution.strategy.PushDownHelper
 import org.apache.spark.sql.hive.CarbonRelation
 import org.apache.spark.sql.optimizer.CarbonFilters
-import org.apache.spark.sql.sources.{And, BaseRelation, Filter, InsertableRelation, Or}
+import org.apache.spark.sql.sources.{And, BaseRelation, EqualNullSafe, EqualTo, Filter, InsertableRelation, Or}
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CarbonException
 
@@ -37,6 +37,7 @@ import org.apache.carbondata.core.index.IndexFilter
 import org.apache.carbondata.core.indexstore.PartitionSpec
 import org.apache.carbondata.core.metadata.AbsoluteTableIdentifier
 import org.apache.carbondata.core.metadata.schema.table.CarbonTable
+import org.apache.carbondata.core.metadata.schema.table.column.CarbonColumn
 import org.apache.carbondata.core.scan.expression.Expression
 import org.apache.carbondata.core.scan.expression.logical.AndExpression
 import org.apache.carbondata.hadoop.CarbonProjection
@@ -67,57 +68,102 @@ case class CarbonDatasourceHadoopRelation(
 
   override def schema: StructType = tableSchema.getOrElse(carbonRelation.schema)
 
-  private def sortFilter(filter: Filter, ordinal: Int = -1): Filter = {
+  def getStorageOrdinal(filter: Filter): Int = {
+    val leftColumn = filter.references.map(leftRef => carbonTable
+      .getCreateOrderColumn
+      .asScala
+      .find(_.getColName.equals(leftRef))).head.get
+    if (leftColumn.isDimension) {
+      leftColumn.getOrdinal
+    } else {
+      leftColumn.getOrdinal + carbonTable.getAllDimensions.size()
+    }
+  }
+
+  def collectSimilarExpressions(filter: Filter): Seq[(Filter, Int)] = {
+    filter match {
+      case And(left, right) =>
+        collectSimilarExpressions(left) ++ collectSimilarExpressions(right)
+      case Or(left, right) => collectSimilarExpressions(left) ++ collectSimilarExpressions(right)
+      case others => Seq((others, getStorageOrdinal(others)))
+    }
+  }
+
+  private def sortFilter(filter: Filter): (Filter, Int) = {
     filter match {
       case and@And(left, right) =>
-        if (left.references.length == 1 && right.references.length == 1) {
-          val leftColumn = left.references.map(leftRef => carbonTable
-            .getCreateOrderColumn
-            .asScala
-            .find(_.getColName.equals(leftRef))).head.get
-          val rightColumn = right.references.map(rightRef => carbonTable
-            .getCreateOrderColumn
-            .asScala
-            .find(_.getColName.equals(rightRef))).head.get
-          if (!(leftColumn.isDimension ^ rightColumn.isDimension)) {
-            if (leftColumn.getOrdinal > rightColumn.getOrdinal) {
-              And(right, left)
+        val hasOr = and.productIterator.exists(_.isInstanceOf[Or])
+        if (hasOr) {
+          if (left.references.length == 1 && right.references.length == 1) {
+            val leftOrdinal = getStorageOrdinal(left)
+            val rightOrdinal = getStorageOrdinal(right)
+            if (leftOrdinal > rightOrdinal) {
+              (And(right, left), leftOrdinal)
             } else {
-              and
+              (and, rightOrdinal)
             }
-          } else if (leftColumn.isMeasure && rightColumn.isDimension) {
-            And(right, left)
           } else {
-            and
+            if (left.references.toSeq == right.references.toSeq ||
+                right.references.diff(left.references).length == 0) {
+              val sorted = sortFilter(left)
+              val rightOrdinal = getStorageOrdinal(right)
+              if (sorted._2 >= rightOrdinal) {
+                (And(right, sorted._1), rightOrdinal)
+              } else {
+                (And(sorted._1, right), sorted._2)
+              }
+            } else {
+              val leftSorted = sortFilter(left)
+              val rightSorted = sortFilter(right)
+              if (leftSorted._2 > rightSorted._2) {
+                (And(rightSorted._1, leftSorted._1), rightSorted._2)
+              } else {
+                (And(leftSorted._1, rightSorted._1), leftSorted._2)
+              }
+            }
           }
         } else {
-          And(sortFilter(left, -1), sortFilter(right, -1))
+          val andFilterList = collectSimilarExpressions(and)
+          val sortedFilterAndOrdinal = andFilterList.sortBy(_._2)
+          (sortedFilterAndOrdinal.map(_._1).reduce(And), sortedFilterAndOrdinal.head._2)
         }
       case or@Or(left, right) =>
-        if (left.references.length == 1 && right.references.length == 1) {
-          val leftColumn = left.references.map(leftRef => carbonTable
-            .getCreateOrderColumn
-            .asScala
-            .find(_.getColName.equals(leftRef))).head.get
-          val rightColumn = right.references.map(rightRef => carbonTable
-            .getCreateOrderColumn
-            .asScala
-            .find(_.getColName.equals(rightRef))).head.get
-          if (!(leftColumn.isDimension ^ rightColumn.isDimension)) {
-            if (leftColumn.getOrdinal > rightColumn.getOrdinal) {
-              Or(right, left)
+        val hasAnd = or.productIterator.exists(_.isInstanceOf[And])
+        if (hasAnd) {
+          if (left.references.length == 1 && right.references.length == 1) {
+            val leftOrdinal = getStorageOrdinal(left)
+            val rightOrdinal = getStorageOrdinal(right)
+            if (leftOrdinal > rightOrdinal) {
+              (Or(right, left), leftOrdinal)
             } else {
-              or
+              (or, rightOrdinal)
             }
-          } else if (leftColumn.isMeasure && rightColumn.isDimension) {
-            Or(right, left)
           } else {
-            or
+            if (left.references.toSeq == right.references.toSeq ||
+                right.references.diff(left.references).length== 0) {
+              val sorted = sortFilter(left)
+              val rightOrdinal = getStorageOrdinal(right)
+              if (sorted._2 >= rightOrdinal) {
+                (Or(right, sorted._1), rightOrdinal)
+              } else {
+                (Or(sorted._1, right), sorted._2)
+              }
+            } else {
+              val leftSorted = sortFilter(left)
+              val rightSorted = sortFilter(right)
+              if (leftSorted._2 > rightSorted._2) {
+                (Or(rightSorted._1, leftSorted._1), rightSorted._2)
+              } else {
+                (Or(leftSorted._1, rightSorted._1), leftSorted._2)
+              }
+            }
           }
         } else {
-          Or(sortFilter(left, -1), sortFilter(right, -1))
+          val orFilterList = collectSimilarExpressions(or)
+          val sortedFilterAndOrdinal = orFilterList.sortBy(_._2)
+          (sortedFilterAndOrdinal.map(_._1).reduce(Or), sortedFilterAndOrdinal.head._2)
         }
-      case others => others
+      case others => (others, getStorageOrdinal(others))
     }
   }
 
@@ -126,8 +172,7 @@ case class CarbonDatasourceHadoopRelation(
       projects: Seq[NamedExpression],
       filters: Array[Filter],
       partitions: Seq[PartitionSpec]): RDD[InternalRow] = {
-    val d = filters.map(sortFilter(_))
-
+    val d = filters.map(sortFilter).sortBy(_._2).map(_._1)
     val filterExpression = d.flatMap { filter =>
       CarbonFilters.createCarbonFilter(schema, filter,
         carbonTable.getTableInfo.getFactTable.getTableProperties.asScala)
--- integration/spark/src/main/scala/org/apache/spark/util/PartitionCache.scala
+++ integration/spark/src/main/scala/org/apache/spark/util/PartitionCache.scala
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,8 +15,6 @@
  * limitations under the License.
  */
 
-=======
->>>>>>> f8cbea987... added cache for segmentFile reading
 package org.apache.spark.util
 
 import java.net.URI
